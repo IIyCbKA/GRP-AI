4253e3756f8d0a93296f2d2050569002252d1855
IIyCbKA <alex.chernobrovenko2005@yandex.ru>
2025-06-16 23:14:27 +0300

    added db models to admin

--- server/gross_regional_product/admin.py
+++ server/gross_regional_product/admin.py
@@ -1,1 +1,25 @@
 from django.contrib import admin
+from .models import Regions, Parameters, Data
+
+@admin.register(Regions)
+class RegionsAdmin(admin.ModelAdmin):
+    list_display       = ('id', 'name', 'created_at')
+    search_fields      = ('name',)
+    ordering           = ('id',)
+
+
+@admin.register(Parameters)
+class ParametersAdmin(admin.ModelAdmin):
+    list_display       = ('id', 'name', 'measure')
+    search_fields      = ('name',)
+    list_filter        = ('measure',)
+    ordering           = ('id',)
+
+
+@admin.register(Data)
+class DataAdmin(admin.ModelAdmin):
+    list_display        = ('id', 'region', 'parameter', 'date', 'value')
+    list_select_related = ('region', 'parameter')
+    search_fields       = ('region__name', 'parameter__name')
+    list_filter         = ('region', 'parameter', 'date')
+    ordering            = ('id',)
--- server/gross_regional_product/models.py
+++ server/gross_regional_product/models.py
@@ -7,11 +7,17 @@ class Regions(models.Model):
   name = models.CharField(max_length=255)
   created_at = models.DateTimeField(auto_now_add=True)
 
+  def __str__(self):
+    return self.name
+
 
 class Parameters(models.Model):
   id = models.BigIntegerField(primary_key=True, unique=True)
   name = models.CharField(max_length=511, unique=True)
   measure = models.CharField(max_length=255)
+
+  def __str__(self):
+    return self.name
 
 
 class Data(models.Model):
@@ -31,3 +37,6 @@ class Data(models.Model):
 
   class Meta:
     unique_together = [('region', 'parameter', 'date')]
+
+  def __str__(self):
+    return f"{self.region} - {self.parameter} {self.date}: {self.value}"
