3c9b648c6284a1326aafa3bfe62f11a1bb014fe7
IIyCbKA <alex.chernobrovenko2005@yandex.ru>
2025-06-19 21:13:38 +0300

    updated styles of chart

--- client/src/features/Regions/RegionInfo/Chart/Chart.tsx
+++ client/src/features/Regions/RegionInfo/Chart/Chart.tsx
@@ -7,11 +7,14 @@ import {
   CartesianGrid,
   Tooltip,
   ResponsiveContainer,
+  Legend,
 } from "recharts";
 import { ChartProps } from "./Chart.interface";
 import styles from "./chart.module.css";
 
 export default function Chart({ title, data }: ChartProps): React.ReactElement {
+  const hasPrediction = data.some((p) => p.prediction !== undefined);
+
   return (
     <div className={styles.chartWrap}>
       <span className={styles.titleZone}>{title}</span>
@@ -21,16 +24,36 @@ export default function Chart({ title, data }: ChartProps): React.ReactElement {
             data={data}
             margin={{
               top: 5,
-              right: 30,
-              left: 5,
+              right: 35,
+              left: 10,
               bottom: 5,
             }}
           >
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="year" />
-            <YAxis dataKey="value" />
+            <XAxis dataKey="year" tickMargin={8} />
+            <YAxis type="number" domain={["auto", "auto"]} tickMargin={8} />
             <Tooltip />
-            <Line type="monotone" dataKey="value" stroke="#8884d8" />
+            <Line
+              name="Фактическое"
+              type="monotone"
+              dataKey="value"
+              stroke="#8884d8"
+            />
+            {hasPrediction && (
+              <Line
+                name="Спрогнозированное"
+                key="prediction"
+                type="monotone"
+                dataKey="prediction"
+                stroke="#278664"
+                isAnimationActive
+              />
+            )}
+            <Legend
+              verticalAlign="bottom"
+              height={36}
+              wrapperStyle={{ lineHeight: "36px" }}
+            />
           </LineChart>
         </ResponsiveContainer>
       </div>
